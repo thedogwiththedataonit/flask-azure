4812b3d2af56c0001216b7dfa7c5b4025ecca6e2
thomas <parkaph@bc.edu>
2021-08-06 12:32:24 -0400

    final revisions

--- app.py
+++ app.py
@@ -2,12 +2,12 @@ from flask import Flask, render_template
 
 
 
-application = Flask(__name__)
+app = Flask(__name__)
 
-@application.route('/')
+@app.route('/')
 def main():
     return render_template('index.html')
 
 
 if __name__ == "__main__":
-    application.run(debug=True) #turn debug off for prodcution deployment
+    app.run(port="5000") #turn debug off for prodcution deployment
